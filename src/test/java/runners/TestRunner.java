2f1ec02d3f3f22b70b4e81e856984d7470e8beeb
Luis <luis.fellipe1217@gmail.com>
2025-03-19 13:15:49 -0300

    Refactoring some features

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -14,7 +14,7 @@ import support.hooks.Hooks;
 import java.io.IOException;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/main/resources/features"}, glue = "steps")
+@CucumberOptions(features = {"src/main/resources/features"}, glue = "steps", plugin = {"pretty", "html:target/cucumber-reports"})
 public class TestRunner extends AbstractTestNGCucumberTests {
     private static final Logger logger = LogManager.getLogger(TestRunner.class);
 
