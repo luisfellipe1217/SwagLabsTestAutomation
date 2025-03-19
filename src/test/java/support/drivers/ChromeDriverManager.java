f07c9b273facfaeb25d15ed10f338e5730f9eb94
Luis <luis.fellipe1217@gmail.com>
2025-03-19 10:59:59 -0300

    Adding chromeoptions

--- src/test/java/support/drivers/ChromeDriverManager.java
+++ src/test/java/support/drivers/ChromeDriverManager.java
@@ -3,6 +3,7 @@ package support.drivers;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class ChromeDriverManager {
     private static WebDriver driver = null;
@@ -13,8 +14,14 @@ public class ChromeDriverManager {
     }
 
     public static synchronized WebDriver getInstance() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
+
         if (driver == null) {
-            driver = new ChromeDriver();
+            driver = new ChromeDriver(options);
         }
         return driver;
     }
